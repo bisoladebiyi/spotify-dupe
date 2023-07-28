6ffcd43c34240f5f0a7dcc2769c02b6a8e54adaa
Abisola Adebiyi <abisola.ca@gmail.com>
2023-07-28 01:00:31 -0700

    create UI for liked songs page

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { OhVueIcon, addIcons } from "oh-vue-icons";
-import { CoHeart, FaHeart } from "oh-vue-icons/icons";
+import { CoHeart, FaHeart, MdAccesstime } from "oh-vue-icons/icons";
 import './index.css'
 
 
-addIcons(CoHeart, FaHeart);
+addIcons(CoHeart, FaHeart, MdAccesstime);
 
 createApp(App).use(router).component("v-icon", OhVueIcon).mount('#app')
